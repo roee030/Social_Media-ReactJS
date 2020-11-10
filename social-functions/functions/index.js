3cc0e45105a551144e04e7fe72cdb7b4f575ce2c
Roee Angel <roee.angel93@gmail.com>
2020-11-10 18:19:21 +0200

    add more end point to the server

--- social-functions/functions/index.js
+++ social-functions/functions/index.js
@@ -3,18 +3,44 @@ const express = require("express");
 const app = express();
 //Import Functions
 const FBAuth = require("./util/FBAuth");
-const { getAllScreams, postOneScream } = require("./handlers/screams");
-const { login, signup, uploadImage } = require("./handlers/users");
-
+const cors = require("cors");
+app.use(cors());
+const {
+  getAllScreams,
+  postOneScream,
+  getScream,
+  commentOnScream,
+  likeScream,
+  unlikeScream,
+  deleteScream,
+} = require("./handlers/screams");
+const {
+  signup,
+  login,
+  uploadImage,
+  addUserDetails,
+  getAuthenticatedUser,
+  getUserDetails,
+  markNotificationsRead,
+} = require("./handlers/users");
 // Initialize Firebase for Auth
 
-//Scream Routes
+// Scream routes
 app.get("/screams", getAllScreams);
 app.post("/scream", FBAuth, postOneScream);
+app.get("/scream/:screamId", getScream);
+app.delete("/scream/:screamId", FBAuth, deleteScream);
+app.get("/scream/:screamId/like", FBAuth, likeScream);
+app.get("/scream/:screamId/unlike", FBAuth, unlikeScream);
+app.post("/scream/:screamId/comment", FBAuth, commentOnScream);
 
-//Users Routes
-app.post("/login", login);
+// Users routes
 app.post("/signup", signup);
+app.post("/login", login);
 app.post("/user/image", FBAuth, uploadImage);
+app.post("/user", FBAuth, addUserDetails);
+app.get("/user", FBAuth, getAuthenticatedUser);
+app.get("/user/:handle", getUserDetails);
+app.post("/notifications", FBAuth, markNotificationsRead);
 
 exports.api = functions.https.onRequest(app);
