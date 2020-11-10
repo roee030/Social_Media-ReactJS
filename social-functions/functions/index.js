175b0ebf4e0f0cf88d1a98a6684f83b0c497e38d
Roee Angel <roee.angel93@gmail.com>
2020-11-10 14:33:24 +0200

    add middleware before post new scream

--- social-functions/functions/index.js
+++ social-functions/functions/index.js
@@ -36,10 +36,45 @@ app.get("/screams", (req, res) => {
     .catch((e) => console.log(e));
 });
 
-app.post("/screams", (req, res) => {
+const FBAuth = (req, res, next) => {
+  let idToken;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer ")
+  ) {
+    idToken = req.headers.authorization.split("Bearer ")[1];
+  } else {
+    console.error("No token found");
+    return res.status(403).json({ error: "Unauthorized" });
+  }
+
+  admin
+    .auth()
+    .verifyIdToken(idToken)
+    .then((decodedToken) => {
+      req.user = decodedToken;
+      return db
+        .collection("users")
+        .where("userId", "==", req.user.uid)
+        .limit(1)
+        .get();
+    })
+    .then((data) => {
+      //   req.user.handle = data.docs[0].data().handle;
+      //   req.user.imageUrl = data.docs[0].data().imageUrl;
+      return next();
+    })
+    .catch((err) => {
+      console.error("Error while verifying token ", err);
+      return res.status(403).json(err);
+    });
+};
+
+//post one scream
+app.post("/scream", FBAuth, (req, res) => {
   const newScream = {
     body: req.body.body,
-    userHandle: req.body.userHandle,
+    userHandle: req.user.handle,
     creatAt: new Date().toISOString(),
   };
   db.collection("screams")
