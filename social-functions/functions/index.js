2de0752f76bbef3baff04c744d8404eb0c7651af
Roee Angel <roee.angel93@gmail.com>
2020-11-10 11:43:47 +0200

    new function to fire base - sign up

--- social-functions/functions/index.js
+++ social-functions/functions/index.js
@@ -3,11 +3,22 @@ const admin = require("firebase-admin");
 admin.initializeApp();
 const express = require("express");
 const app = express();
-
+const firebaseConfig = {
+  apiKey: "AIzaSyBqzYuCxoU0-3_i6qfhxS1sS4GgdTZUnrI",
+  authDomain: "socialape-dda8e.firebaseapp.com",
+  databaseURL: "https://socialape-dda8e.firebaseio.com",
+  projectId: "socialape-dda8e",
+  storageBucket: "socialape-dda8e.appspot.com",
+  messagingSenderId: "993373409509",
+  appId: "1:993373409509:web:235c8b475577018146440c",
+  measurementId: "G-EM1JPX95F9",
+};
+const firebase = require("firebase");
+// Initialize Firebase for Auth
+firebase.initializeApp(firebaseConfig);
+const db = admin.firestore();
 app.get("/screams", (req, res) => {
-  admin
-    .firestore()
-    .collection("screams")
+  db.collection("screams")
     .orderBy("createdAt", "desc")
     .get()
     .then((data) => {
@@ -31,9 +42,7 @@ app.post("/screams", (req, res) => {
     userHandle: req.body.userHandle,
     creatAt: new Date().toISOString(),
   };
-  admin
-    .firestore()
-    .collection("screams")
+  db.collection("screams")
     .add(newScream)
     .then((data) => {
       res.json({ message: `document ${data.id} created successfully` });
@@ -43,5 +52,61 @@ app.post("/screams", (req, res) => {
       console.log(e);
     });
 });
+//create new user using firebase
+app.post("/signup", (req, res) => {
+  const newUser = {
+    email: req.body.email,
+    password: req.body.password,
+    confirmPassword: req.body.confirmPassword,
+    handle: req.body.handle,
+  };
 
+  //   const { valid, errors } = validateSignupData(newUser);
+
+  //   if (!valid) return res.status(400).json(errors);
+
+  const noImg = "no-img.png";
+
+  let token, userId;
+  db.doc(`/users/${newUser.handle}`)
+    .get()
+    .then((doc) => {
+      if (doc.exists) {
+        return res.status(400).json({ handle: "this handle is already taken" });
+      } else {
+        return firebase
+          .auth()
+          .createUserWithEmailAndPassword(newUser.email, newUser.password);
+      }
+    })
+    .then((data) => {
+      userId = data.user.uid;
+      return data.user.getIdToken();
+    })
+    .then((idToken) => {
+      token = idToken;
+      const userCredentials = {
+        handle: newUser.handle,
+        email: newUser.email,
+        createdAt: new Date().toISOString(),
+        //TODO Append token to imageUrl. Work around just add token from image in storage.
+        imageUrl: `https://firebasestorage.googleapis.com/v0/b/${config.storageBucket}/o/${noImg}?alt=media`,
+        userId,
+      };
+      return db.doc(`/users/${newUser.handle}`).set(userCredentials);
+    })
+    .then(() => {
+      return res.status(201).json({ token });
+    })
+    .catch((err) => {
+      console.error(err);
+      if (err.code === "auth/email-already-in-use") {
+        return res.status(400).json({ email: "Email is already is use" });
+      } else {
+        return res
+          .status(500)
+          .json({ general: "Something went wrong, please try again" });
+      }
+    });
+});
 exports.api = functions.https.onRequest(app);
