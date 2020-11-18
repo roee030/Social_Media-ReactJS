cfadcdb6fc437a0879d0f448024f2caa96127803
Roee Angel <roee.angel93@gmail.com>
2020-11-18 22:20:16 +0200

    add validation

--- social-functions/functions/handlers/users.js
+++ social-functions/functions/handlers/users.js
@@ -43,9 +43,9 @@ exports.signup = (req, res) => {
     handle: req.body.handle,
   };
 
-  //   const { valid, errors } = validateSignupData(newUser);
+  const { valid, errors } = validateSignupData(newUser);
 
-  //   if (!valid) return res.status(400).json(errors);
+  if (!valid) return res.status(400).json(errors);
 
   const noImg = "no-img.png";
 
